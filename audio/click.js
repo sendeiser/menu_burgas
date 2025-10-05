565d02376ac0a293f4c25aac4c208d169663a80e
sendeiser <martingt010@gmail.com>
2025-10-05 06:44:41 -0300

    sonido click mute

--- audio/click.js
+++ audio/click.js
@@ -5,7 +5,7 @@ const clickSoundData = {
   name: "Button Click Sound",
   format: "mp3",
   // URL directa al archivo MP3 de un efecto de sonido de clic
-  url: "https://cdn.pixabay.com/download/audio/2022/03/15/audio_270f49b303.mp3"
+  url: ""
 };
 
 export default clickSoundData;
